c213fc4a2db5130b9dec6708c16981deb41a2696
Nikhil Sahukar <nsahukar@gmail.com>
2023-07-24 18:29:18 +0530

    Added a test for a missing player
    Wrote minimal code to make this test pass. It highlighted that other
    two tests are not asserting for http.StatusOK.

--- server.go
+++ server.go
@@ -16,6 +16,7 @@ type PlayerServer struct {
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, p.Store.GetPlayerScore(player))
 }
 
