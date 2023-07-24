780593a138321d4b3a2b032741aacc634fc928d9
Nikhil Sahukar <nsahukar@gmail.com>
2023-07-24 22:56:19 +0530

    Added integration test b/w InMemoryPlayerStore and PlayerServer
    - Implemented a quick and simple in memory store using map.
    - Used this store in the integration test and also in main.go.

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -6,16 +6,8 @@ import (
 	"server"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {}
-
 func main() {
 	// handler := http.HandlerFunc(server.PlayerServer)
-	server := &server.PlayerServer{Store: &InMemoryPlayerStore{}}
-	log.Fatal(http.ListenAndServe(":5000", server))
+	playerServer := &server.PlayerServer{Store: server.NewInMemoryPlayerStore()}
+	log.Fatal(http.ListenAndServe(":5000", playerServer))
 }
---
+++ server_integration_test.go
@@ -0,0 +1,23 @@
+package server_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"server"
+	"testing"
+)
+
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store := server.NewInMemoryPlayerStore()
+	server := server.PlayerServer{store}
+	player := "Pepper"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostScoreRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostScoreRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostScoreRequest(player))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+	assertStatus(t, response.Code, http.StatusOK)
+	assertResponseBody(t, response.Body.String(), "3")
+}
