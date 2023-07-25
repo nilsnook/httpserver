e1f4a060e2d05c872df639ff86546c9d22d2bd51
Nikhil Sahukar <nsahukar@gmail.com>
2023-07-25 11:02:52 +0530

    Added test for new path /league to assert status OK response
    - Now using `http.NewServeMux()` as router.
    - Two paths exist now - /league and /players/.

--- server_test.go
+++ server_test.go
@@ -118,3 +118,17 @@ func newPostScoreRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodPost, fmt.Sprintf("/players/%s", name), nil)
 	return req
 }
+
+func TestLeague(t *testing.T) {
+	store := StubPlayerStore{}
+	server := &server.PlayerServer{&store}
+
+	t.Run("it returns 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
