f2133a9b4471973eae0e9a12b73795d8717dd4b8
Nikhil Sahukar <nsahukar@gmail.com>
2023-07-26 12:50:14 +0530

    Added test to assert GetPlayerScore for file-based store

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -7,21 +7,34 @@ import (
 )
 
 func TestFileSystemStore(t *testing.T) {
-	t.Run("league from a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+	database := strings.NewReader(`[
 			{"Name": "Cleo", "Wins": 10},
 			{"Name": "Chris", "Wins": 33}
 		]`)
-		store := server.FileSystemPlayerStore{database}
+	store := server.FileSystemPlayerStore{database}
 
-		got := store.GetLeague()
+	t.Run("league from a reader", func(t *testing.T) {
 		want := []server.Player{
 			{"Cleo", 10},
 			{"Chris", 33},
 		}
+		got := store.GetLeague()
 		assertLeague(t, want, got)
 
 		got = store.GetLeague()
 		assertLeague(t, want, got)
 	})
+
+	t.Run("get player score", func(t *testing.T) {
+		want := 33
+		got := store.GetPlayerScore("Chris")
+		assertScoreEquals(t, want, got)
+	})
+}
+
+func assertScoreEquals(t testing.TB, want, got int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
 }
